f956c4536028f3d3bf206b01cb792e74b3694d8f
Shilajit2002 <shilajit.acharjee@gmail.com>
2024-06-22 16:52:35 +0530

    BaseUrl Updated

--- frontend/src/Helper/BaseUrl.js
+++ frontend/src/Helper/BaseUrl.js
@@ -1,8 +1,9 @@
-// const backendURL = "https://webdualityweb.onrender.com/api/v1"
+const backendURL = "https://webdualityweb.onrender.com/api/v1"
 // const backendURL = "https://webdualityweb.onrender.com/v1"
 // const backendURL = "http://54.205.120.140:8000/api/v1"
-const backendURL = "https://webduality.com/api/v1"
+// const backendURL = "https://webduality.com/api/v1"
 // const frontendURL = "http://localhost:3000"
-const frontendURL = "https://webduality.com/"
+// const frontendURL = "https://webduality.com/"
+const frontendURL = "https://webduality.in/"
 
 export { backendURL, frontendURL }
