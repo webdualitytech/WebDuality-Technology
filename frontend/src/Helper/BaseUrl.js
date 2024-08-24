d436135828e55bbf3149f240993f4e65114f4ebe
Shilajit2002 <shilajit.acharjee@gmail.com>
2024-08-24 21:37:08 +0530

    Server URL updated

--- frontend/src/Helper/BaseUrl.js
+++ frontend/src/Helper/BaseUrl.js
@@ -1,12 +1,11 @@
-const backendURL = "https://webdualityweb.onrender.com/api/v1"
-// const backendURL = "https://webdualityweb.onrender.com/v1"
+const backendURL = "https://webduality-technologies-official-server.onrender.com/api/v1"
 // const backendURL = "http://54.205.120.140:8000/api/v1"
 // const backendURL = "https://webduality.com/api/v1"
 // const frontendURL = "http://localhost:3000"
 // const frontendURL = "https://webduality.com/"
 const frontendURL = "https://webduality.in/"
 
-const BACKEND_SERVER = "https://webduality-task-tracker.onrender.com/api/v1";
+const BACKEND_SERVER = "https://webduality-technologies-powersheet-server.onrender.com/api/v1";
 
 const POWER_SHEET_ROUTE = "/webduality-technologies-power-sheet/N9UrrLe3pVgwCZ9jtdXj"
 
